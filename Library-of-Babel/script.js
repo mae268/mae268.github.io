ddf04deec4e31985dc499a501832e1eabf221c4f
mae268 <mae268@newschool.edu>
2021-03-02 09:36:38 -0500

    update

--- Library-of-Babel/script.js
+++ Library-of-Babel/script.js
@@ -1,6 +1,32 @@
 
 $(document).ready(function() {
 
+	window.addEventListener('scroll', () => {
+  	document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
+
+// let left = document.querySelector('#left');
+// let right = document.querySelector('#right');
+
+// let options = {
+//   rootMargin: '0px',
+//   threshold: 1
+// };
+
+// function animate(entries, observer) {
+//   for(let entry of entries) {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('left');
+//       entry.target.classList.add('right');
+//     }
+//   }
+// }
+
+// let observer = new IntersectionObserver(animate, options);
+
+// observer.observe(left);
+
+// observer.observe(right);
 
 
 
