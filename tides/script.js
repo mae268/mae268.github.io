ff1e1d4fe49986982b10f1dbdb108325b9876398
mae268 <mae268@newschool.edu>
2021-04-15 18:57:57 -0400

    update

--- tides/script.js
+++ tides/script.js
@@ -4,14 +4,24 @@ let circle = document.querySelector('#circle');
 
 let ocean = document.querySelector('#ocean');
 
-function showNight() {
-     document.body.style.color = 'mediumslateblue';
-     document.circle.style.color = 'white';
-}
+let isColored = false;
 
-circle.addEventListener('click', showNight);
+//I declared a Boolean variable “isColored”. I’ve made it false so when the page refreshes then it will make the background lightcyan, and the circle yellow. Then in the callback function, I created an if else loop which will check the background color, whether it is lightcyan or darkslateblue, and the color of the circle (yellow or cornsilk), then it will change the color.
+circle.addEventListener('click', function() {
+	if(isColored) {
+		document.body.style.background = 'paleturquoise';
+		circle.style.backgroundColor = 'yellow';
+		isColored = false
+	} else {
+		document.body.style.background = 'mediumslateblue';
+		circle.style.backgroundColor = 'cornsilk';
+		isColored = true;
+	}
+});
 
-function moveOcean() {
-	document.ocean.animate
-}
-circle.addEventListener('mouseover', moveOcean);
+//attempting to figure out how to trigger animations below
+// function moveOcean() {
+// 	document.ocean.animate('ocean');
+// }
+
+// circle.addEventListener('mouseover', moveOcean);
