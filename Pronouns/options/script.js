d5ab75569d7edd4f12ba59f8a1d4f73c74a90b0e
mae268 <mae268@newschool.edu>
2021-05-06 20:53:37 -0400

    update

--- Pronouns/options/script.js
+++ Pronouns/options/script.js
@@ -1,7 +1,19 @@
+let talk = document.querySelector('#talk');
+let write = document.querySelector('#write');
 
-$(document).ready(function() {
+talk.onmouseover = function () {
+	talk.innerHTML = "talk";
+};
 
+write.onmouseover = function () {
+	write.innerHTML = "write";
+};
 
+talk.onmouseout = function () {
+  talk.innerHTML = "说";
+};
 
+write.onmouseout = function () {
+  write.innerHTML = "写";
+};
 
-});
--- Pronouns/talk/script.js
+++ Pronouns/talk/script.js
@@ -1,7 +1,7 @@
 let textInput = document.querySelector('#text-input');
 let body = document.querySelector('body');
 let howtowrite = document.querySelector('howtowrite');
-
+let one = document.querySelector('#q-one');
 
 let nameEntered = false;
 let pronounsEntered = false;
@@ -13,6 +13,14 @@ let personInfo = {
   'name': '',
   'pronouns': '',
 }
+
+one.onmouseover = function () {
+	one.innerHTML = "Hi, what's your name?";
+};
+
+one.onmouseout = function () {
+  one.innerHTML = "你好, 你叫什么名字?";
+};
 
 
 function textEntry(event) {
@@ -35,6 +43,14 @@ function textEntry(event) {
       askPronouns.innerText = "什么是你的性别认同?";
       body.append(askPronouns);
 
+      askPronouns.onmouseover = function () {
+		askPronouns.innerHTML = "What is your gender identity?";
+	};
+
+	askPronouns.onmouseout = function () {
+  	askPronouns.innerHTML = "什么是你的性别认同?";
+	};
+
       textInput.value = '';
 
     } else if (!pronounsEntered) {
@@ -51,6 +67,14 @@ function textEntry(event) {
       response.innerText = "好, 知道了!";
       body.append(response);
 
+       response.onmouseover = function () {
+		response.innerHTML = "Ok, got it!";
+		};
+
+		response.onmouseout = function () {
+  		response.innerHTML = "好, 知道了!";
+		};
+
       textInput.value = '';
 
       let explain = document.createElement('div');
@@ -58,15 +82,40 @@ function textEntry(event) {
       explain.innerText = "我们说中文的时候不需要担心代词, 因为男女的代词发因是一样的";
       body.append(explain);
 
+
+       explain.onmouseover = function () {
+		explain.innerHTML = "When we speak Chinese you don't have to worry about pronouns, because all pronouns sound the same";
+		};
+
+		explain.onmouseout = function () {
+  		explain.innerHTML = "我们说中文的时候不需要担心代词, 因为男女的代词发因是一样的";
+		};
+
 		let understood = document.createElement('button');
 		understood.classList.add('understood');
 		understood.innerText = "明白了";
 		body.append(understood);
 
+		understood.onmouseover = function () {
+			understood.innerHTML = "understood"
+		};
+
+		understood.onmouseout = function () {
+			understood.innerHTML = "明白了";
+		};
+
 		let howtowrite = document.createElement('button');
 		howtowrite.classList.add('howtowrite');
 		howtowrite.innerText = "写怎么办?";
 		body.append(howtowrite);
+
+		howtowrite.onmouseover = function () {
+			howtowrite.innerHTML = "what about writing?";
+		};
+
+		howtowrite.onmouseout = function () {
+			howtowrite.innerHTML = "写怎么办?";
+		};
 
 		howtowrite.onclick = function() {
 		window.location.replace('../write/index.html')
